786da0c2e2c1edceef912d149f4fd74da18d6acb
Jeff Loughridge <jeffl@konekti.us>
2019-10-13 19:50:31 +0000

    Upgrade to TF 0.12 and pin terraform-aws-vpc module to 2.17.0

--- vpc-peering-same-account/vpcs.tf
+++ vpc-peering-same-account/vpcs.tf
@@ -1,12 +1,12 @@
 module "vpc-west" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "1.53.0"
+  version = "2.17.0"
 
   name = "terraform-vpc-west"
 
   cidr = "10.0.0.0/16"
 
-  azs            = ["us-west-1a", "us-west-1b"]
+  azs            = ["us-east-1a", "us-east-1b"]
   public_subnets = ["10.0.0.0/24", "10.0.1.0/24"]
 
   enable_dns_hostnames = true
@@ -15,13 +15,13 @@ module "vpc-west" {
 
 module "vpc-east" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "1.53.0"
+  version = "2.17.0"
 
   name = "terraform-vpc-east"
 
   cidr = "10.1.0.0/16"
 
-  azs            = ["us-west-1a", "us-west-1b"]
+  azs            = ["us-east-1a", "us-east-1b"]
   public_subnets = ["10.1.0.0/24", "10.1.1.0/24"]
 
   enable_dns_hostnames = true
@@ -29,8 +29,8 @@ module "vpc-east" {
 }
 
 resource "aws_vpc_peering_connection" "pc" {
-  peer_vpc_id = "${module.vpc-west.vpc_id}"
-  vpc_id      = "${module.vpc-east.vpc_id}"
+  peer_vpc_id = module.vpc-west.vpc_id
+  vpc_id      = module.vpc-east.vpc_id
   auto_accept = true
 
   accepter {
@@ -48,14 +48,15 @@ resource "aws_vpc_peering_connection" "pc" {
 
 resource "aws_route" "vpc-peering-route-east" {
   count                     = 2
-  route_table_id            = "${module.vpc-east.public_route_table_ids[0]}"
-  destination_cidr_block    = "${module.vpc-west.public_subnets_cidr_blocks[count.index]}"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.pc.id}"
+  route_table_id            = module.vpc-east.public_route_table_ids[0]
+  destination_cidr_block    = module.vpc-west.public_subnets_cidr_blocks[count.index]
+  vpc_peering_connection_id = aws_vpc_peering_connection.pc.id
 }
 
 resource "aws_route" "vpc-peering-route-west" {
   count                     = 2
-  route_table_id            = "${module.vpc-west.public_route_table_ids[0]}"
-  destination_cidr_block    = "${module.vpc-east.public_subnets_cidr_blocks[count.index]}"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.pc.id}"
+  route_table_id            = module.vpc-west.public_route_table_ids[0]
+  destination_cidr_block    = module.vpc-east.public_subnets_cidr_blocks[count.index]
+  vpc_peering_connection_id = aws_vpc_peering_connection.pc.id
 }
+
