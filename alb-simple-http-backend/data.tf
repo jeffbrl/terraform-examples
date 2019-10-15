c5143b5aaca6bc2dce1c10a3f29dabbfc2c1af2f
Jeff Loughridge <jeffl@konekti.us>
2019-10-15 14:28:56 +0000

    Add alb-simple-http-backend example

---
+++ alb-simple-http-backend/data.tf
@@ -0,0 +1,41 @@
+data "aws_availability_zones" "available" {
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+
+/*
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+*/
