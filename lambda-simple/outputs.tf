bd49e7dc33428e883672cca873cefb1820a4c096
Jeff Loughridge <jeffl@konekti.us>
2020-11-26 22:43:46 +0000

    Add simple Lambda example

---
+++ lambda-simple/outputs.tf
@@ -0,0 +1,4 @@
+output "user_message" {
+  value = "aws lambda invoke --function-name ${aws_lambda_function.main.function_name} out --log-type Tail --query 'LogResult' --output text --region ${var.aws_region} |  base64 -d"
+}
+
---
+++ lambda-simple/variables.tf
@@ -0,0 +1,6 @@
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+variable "name" {}
+
