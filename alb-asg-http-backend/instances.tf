6411b2ff19d132a608806c428d5c6d1de8cfcb20
Jeff Loughridge <jeffl@konekti.us>
2025-03-21 08:43:54 -0400

    Convert instance size to variable

--- alb-asg-http-backend/instances.tf
+++ alb-asg-http-backend/instances.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web_instances" {
   ami   = data.aws_ami.amazon_linux_2.id
   count = 2
 
-  instance_type               = "t2.micro"
+  instance_type               = var.instance_size
   associate_public_ip_address = false
   subnet_id                   = module.vpc.private_subnets[count.index]
   security_groups             = [module.vpc.default_security_group_id]
--- alb-asg-http-backend/variables.tf
+++ alb-asg-http-backend/variables.tf
@@ -1,3 +1,8 @@
 variable "aws_region" {
   default = "us-east-1"
 }
+
+variable "instance_size" {
+  description = "AWS EC2 instance size"
+  type        = string
+}
